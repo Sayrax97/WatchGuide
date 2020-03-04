4d906fff9d7803aae20f7df78303f609fae60e2c
Sayrax97 <dusann.jankovic@elfak.rs>
2020-03-04 18:12:13 +0100

    fix film desc

--- server/migrations/20200303141900-create-film.js
+++ server/migrations/20200303141900-create-film.js
@@ -20,7 +20,7 @@ module.exports = {
         allowNull: false
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       release_date: {
         type: Sequelize.DATE,

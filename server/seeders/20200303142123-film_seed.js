dedd7d3ccfe5d1131b90c16f227aba7cc00624b2
Sayrax97 <dusann.jankovic@elfak.rs>
2020-03-08 12:49:53 +0100

    'filmseed'

--- server/seeders/20200303142123-film_seed.js
+++ server/seeders/20200303142123-film_seed.js
@@ -12,7 +12,8 @@ module.exports = {
             "An ancient Ring thought lost for centuries has been found, and through a strange twist of fate has been given to a small Hobbit named Frodo.When Gandalf discovers the Ring is in fact the One Ring of the Dark Lord Sauron, Frodo must make an epic quest to the Cracks of Doom in order to destroy it.However, he does not go alone.He is joined by Gandalf, Legolas the elf, Gimli the Dwarf, Aragorn, Boromir, and his three Hobbit friends Merry, Pippin, and Samwise.Through mountains, snow, darkness, forests, rivers and plains, facing evil and danger at every corner the Fellowship of the Ring must go.Their quest to destroy the One Ring is the only hope for the end of the Dark Lords reign.",
           release_date: "2001-12-19",
           trailer_link: "https://www.youtube.com/watch?v=V75dMMIW2B4",
-          parantial_quide: 13
+          parantial_quide: 13,
+          cover_path:'http://localhost:3000/img/film/1/cover.jpg'
         },
         {
           title: "The Lord of the Rings: The Two Towers",
